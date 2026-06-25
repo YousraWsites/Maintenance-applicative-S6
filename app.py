82acf646f3fab6ce34cc97f5ad8b20616a5175e2
GraceKoffi <grace01koffi@gmail.com>
2026-06-25 14:33:16 +0200

    inprove: ajouter un bouton pour inverser les devises selectionnees

--- app.py
+++ app.py
@@ -21,9 +21,26 @@ def get_rates():
 
 rates = get_rates()
 
+currencies = list(rates.keys())
+
+if "from_currency" not in st.session_state:
+    st.session_state.from_currency = currencies[0]
+if "to_currency" not in st.session_state:
+    st.session_state.to_currency = currencies[1]
+
+
+def swap_currencies():
+    st.session_state.from_currency, st.session_state.to_currency = (
+        st.session_state.to_currency,
+        st.session_state.from_currency,
+    )
+
+
 amount = st.number_input("Montant :", min_value=0.0, format="%.2f")
-from_currency = st.selectbox("De :", rates.keys())
-to_currency = st.selectbox("Vers :", rates.keys())
+from_currency = st.selectbox("De :", currencies, key="from_currency")
+to_currency = st.selectbox("Vers :", currencies, key="to_currency")
+
+st.button("Inverser les devises", on_click=swap_currencies)
 
 
 #AVANT
